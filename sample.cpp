569ce5b880152b92b3ce936b713b88aa609a1720
root <root@DESKTOP-MUVEHV2.localdomain>
2020-10-11 20:48:25 +0900

    ff

--- sample.cpp
+++ sample.cpp
@@ -1,25 +1,57 @@
+// #define _GLIBCXX_DEBUG
+// #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
 using namespace std;
-typedef long long ll;
-const ll MOD = 1e9+7;
-int main(){
-    map<char, int> t;
-    char b;
-    string e, l;
-    cin >> e >> b >> l;
-    for(int i=0;i<5;i++) t[e[i]]++;
+template <class T> using V = vector<T>;
+template <class T> using VV = V<V<T>>;
 
-    int cnt=0;
-    bool bonas=false;
+class Prints {
+private:
+    class __Prints {
+    public:
+        __Prints(const char* _sep, const char* _term) : sep(_sep), term(_term) {}
+        template <class... Args> void operator()(const Args&... args) const { print(args...); }
+        template <typename T> void pvec(const T& vec, size_t sz) const {
+            for (size_t i = 0; i < sz; i++) std::cout << vec[i] << (i == sz - 1 ? term : sep);
+        }
+        template <typename T> void pmat(const T& mat, size_t h, size_t w) {
+            for (size_t i = 0; i < h; i++)
+                for (size_t j = 0; j < w; j++) std::cout << mat[i][j] << (j == w - 1 ? term : sep);
+        }
 
+    private:
+        const char *sep, *term;
+        void print() const { std::cout << term; }
+        void print_rest() const { std::cout << term; }
+        template <class T, class... Tail> void print(const T& head, const Tail&... tail) const {
+            std::cout << head, print_rest(tail...);
+        }
+        template <class T, class... Tail> void print_rest(const T& head, const Tail&... tail) const {
+            std::cout << sep << head, print_rest(tail...);
+        }
+    };
 
-    for(int i=0;i<5;i++){
-        if(t[l[i]] > 0) cnt++;
-        if(l[i] == b) bonas = true;
-    }
+public:
+    Prints() {}
+    __Prints operator()(const char* sep = " ", const char* term = "\n") const { return __Prints(sep, term); }
+};
 
-    
+/* -------------------------------------------------- */
 
+Prints print;
 
-    return 0;
+int main() {
+    int a = 4, b = 5;
+    string S = "abc";
+    print()(a, b, S);
+    print(", 区切り ,", "改行\n")("あ", "いう", "えお");
+    print()();
+
+    V<int> A = {1, 2, 3, 4, 5, 6};
+    print().pvec(A, 6);
+    print()();
+
+    VV<int> mat = {{10, 20, 30}, {40, 50, 60}};
+    print().pmat(mat, 2, 3);
 }
