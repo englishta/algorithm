fa4c7ab3ac9760d8bbfd5a146549e6ba990293eb
root <root@DESKTOP-MUVEHV2.localdomain>
2020-10-13 18:16:36 +0900

    aa

--- note.cpp
+++ note.cpp
@@ -4,7 +4,28 @@ using ll = long long;
 #define rep(i,n) for(ll i=0; i<n; i++)
 
 int main() {
+	ll n;
+	cin >> n;
+	ll min = 1e9;
+	priority_queue<int> que;
 
-
+	rep(i,n){
+		ll a;
+		cin >> a;
+		que.push(a);
+	}
+	while(!que.empty()){
+		ll x = que.top();
+		que.pop();
+		if(x == min){
+			cout << min << '\n';
+			break;
+		}
+		ll y = x%min;
+		if(y){
+			if(min>y) min = y;
+			que.push(y);
+		}
+	}
 	return 0;
 }
