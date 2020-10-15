702f849efd585d4ee69c2b3955ce23c939c35d58
root <root@DESKTOP-MUVEHV2.localdomain>
2020-10-15 23:40:51 +0900

    a

--- note.cpp
+++ note.cpp
@@ -3,80 +3,54 @@ using namespace std;
 using ll = long long;
 #define rep(i,n) for(ll i=0; i<n; i++)
 
-template<class T>
-class SegmentTree {
-
-public:
-    /**
-     * @param N size
-     * @param e identity element
-     * @param operation operation for query
-     * @param updater operation for update
-     */
-    SegmentTree(size_t N, T e, function<T(T, T)> operation, function<T(T, T)> updater)
-            : e(e), operation(std::move(operation)), updater(move(updater)) {
-        n = 1;
-        while (n < N) {
-            n *= 2;
+/* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
+    update(i,x): i 番目の要素を x に更新。O(log(n))
+    query(a,b): [a,b) での最小の要素を取得。O(log(n))
+*/
+template <typename T>
+class RMQ {
+    public:
+    
+    const T INF = numeric_limits<T>::max();
+    int n;         // 葉の数
+    vector<T> dat; // 完全二分木の配列
+    RMQ(int n_) : n(), dat(n_ * 4, INF) { // 葉の数は 2^x の形
+        int x = 1;
+        while (n_ > x) {
+            x *= 2;
         }
-        data = vector<T>(2 * n - 1, e);
+        n = x;
     }
 
-    /**
-     * iの値をxに更新
-     * @param i index ( 0-indexed )
-     * @param x  value
-     */
     void update(int i, T x) {
         i += n - 1;
-        data[i] = updater(data[i], x);
+        dat[i] = x;
         while (i > 0) {
-            i = (i - 1) / 2;
-            data[i] = operation(data[i * 2 + 1], data[i * 2 + 2]);
+            i = (i - 1) / 2;  // parent
+            dat[i] = min(dat[i * 2 + 1], dat[i * 2 + 2]);
         }
     }
 
-    /**
-     * [a, b)の区間でクエリを実行
-     */
-    T query(int a, int b) {
-        return query(a, b, 0, 0, n);
-    }
-
-    /**
-     * 添字でアクセス
-     * @param i index ( 0-indexed )
-     */
-    T operator[](int i) {
-        return data[i + n - 1];
-    }
-
-private:
-    int n;
-    vector<T> data;
-    T e;
-    function<T(T, T)> operation;
-    function<T(T, T)> updater;
-
-    T query(int a, int b, int k, int l, int r) {
-        // 交差しない
-        if (r <= a || b <= l) return e;
-        // 区間 [a, b) に l, r が含まれる
-        if (a <= l && r <= b) return data[k];
-        // 左の子
-        T c1 = query(a, b, 2 * k + 1, l, (l + r) / 2);
-        // 右の子
-        T c2 = query(a, b, 2 * k + 2, (l + r) / 2, r);
-        return operation(c1, c2);
+    // the minimum element of [a,b)
+    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+    T query_sub(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) {
+            return INF;
+        } else if (a <= l && r <= b) {
+            return dat[k];
+        } else {
+            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return min(vl, vr);
+        }
     }
 };
 
 
-
-
-
-
 int main() {
-    vector<int> a={1, 3, 7, 2, 5, 3, 5, 9, 2, 4, 3, 5, 5, 2, 5};
+    int n;
+    cin >> n;
+    RMQ<int> tree(n);
+    cout << tree.n << endl;
 	return 0;
 }
