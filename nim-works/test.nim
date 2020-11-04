975e98cf7088200828bd58635b7869ec707fd3f3
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-04 16:05:48 +0900

    jj

--- nim-works/test.nim
+++ nim-works/test.nim
@@ -1,7 +1,15 @@
 import strutils
-var x = parseInt(readline(stdin))
-echo x
+let read = iterator: string {.closure.} =
+  while true:
+    for s in stdin.readLine.split:
+      yield s
+proc scan() : int =read().parseInt
+proc scanf() : float =read().parseFloat
 
-var k = parseFloat(readline(stdin))
-echo k
+var sum = 0
+let N = scan() 
 
+for i in 0..<N:
+    var a, b = scan() 
+    sum += (a+b)*(b-a+1)div 2
+echo sum
