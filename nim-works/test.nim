ec1ea1ffb3d77d128cb7ee6d707556d98157ed04
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-05 10:50:27 +0900

    aaa

--- nim-works/test.nim
+++ nim-works/test.nim
@@ -4,12 +4,25 @@ let read = iterator: string {.closure.} =
     for s in stdin.readLine.split:
       yield s
 proc scan() : int =read().parseInt
-proc scanf() : float =read().parseFloat
+#proc scanf() : float =read().parseFloat
+#-----------------------------------------------------------------------------------#
+var
+  n = scan()
+  x = newseq[int](n)
+  y = newseq[int](n)
 
-var sum = 0
-let N = scan() 
+for i in 0..<n:
+  (x[i], y[i]) = (scan(), scan())
 
-for i in 0..<N:
-    var a, b = scan() 
-    sum += (a+b)*(b-a+1)div 2
-echo sum
+for i in 0..<n:
+  for j in i+1..<n:
+    for k in j+1..<n:
+      let
+        x2 = x[j]-x[i]
+        y2 = y[j]-y[i] 
+        x3 = x[k]-x[i]
+        y3 = y[k]-y[i] 
+      if (x2*y3 == x3*y2):
+          echo "Yes"
+          quit()
+echo "No"
