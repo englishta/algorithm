257c205da3034c8a687755b703e652a812699f00
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-13 19:17:48 +0900

    commit

--- py.py
+++ py.py
@@ -6,20 +6,3 @@ from bisect import bisect_left, bisect_right, bisect, insort_left, insort_right,
 from itertools import accumulate, product, permutations, combinations, combinations_with_replacement
 # }}} End Heade
 # _________コーディングはここから！！___________
-
-import plotly.graph_objs as go
-import numpy as np
-import plotly.offline as offline
-
-N = 1000
-random_x = np.random.randn(N)
-random_y = np.random.randn(N)
-
-trace = go.Scatter(
-    x = random_x,
-    y = random_y,
-    mode = 'markers'
-)
-
-data = [trace]
-offline.plot(data, filename='data', auto_open=True)
