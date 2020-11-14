ce5feb4faa29750a2cf01978cef06ad97f908b73
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-15 00:07:07 +0900

    commit

--- graphic/kaggle/titanik/ti.py
+++ graphic/kaggle/titanik/ti.py
@@ -1,6 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import graphviz
 import pydotplus
 from IPython.display import Image
-#from sklearn mport .externals.sixStringIO
+import io
 
 
+data = pd.DataFrame({
+        "buy(y)":[True,True,True,True,True,True,True,False,False,False,False,False,False],
+        "high":[4, 5, 3, 1, 6, 3, 4, 1, 2, 1, 1,1,3],
+        "size":[30, 45, 32, 20, 35, 40, 38, 20, 18, 20, 22,24,25],
+        "autolock":[1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0,1,0]
+    })
+
+y = data.loc[:,["buy(y)"]]
+X = data.loc[:,["high", "size","autolock"]]
+
+clf = DecisionTreeClassifier()
+clf = clf.fit(X, y)
+
+dot_data = io.StringIO() #dotファイル情報の格納先
+export_graphviz(clf, out_file=dot_data,  
+                     feature_names=["high", "size","autolock"],#編集するのはここ  
+                     class_names=["False","True"],#編集するのはここ（なぜFase,Trueの順番なのかは後程触れます）  
+                     filled=True, rounded=True,  
+                     special_characters=True) 
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue()) 
+Image(graph.create_png())
