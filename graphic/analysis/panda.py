e13c0c0e19ab63968a4dd54c41d96c8fea6a0c8a
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-19 14:12:02 +0900

    commit

--- graphic/analysis/panda.py
+++ graphic/analysis/panda.py
@@ -24,4 +24,30 @@ print(obj)#objの値は変更されない
 obj22 = obj2*2
 print(obj22)
 
+
+
+
 # %%
+#dictonaryをSeriesに入れる,Keyがインデックス,valueがオブジェクト
+
+sdata={'Ohio':35000, 'Texas':71000, 'Oregon':16000, 'Utah':5000} 
+obj3 = pd.Series(sdata)
+
+obj3
+# %%
+#keyを並べ替えて指定すると、Seriesのオブジェクトも並べ替えられる
+states = ['Texas', 'Utah','Ohio','Oregon']
+obj4 = pd.Series(sdata, index=states)
+obj4
+
+
+# %%
+#Californiaインデックスのオブジェクトは存在しない
+states = ['California', 'Utah','Ohio','Oregon']
+obj4 = pd.Series(sdata, index=states)
+obj4
+
+# %%
+
+
+
