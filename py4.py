bdfae26a7343f00a05d5ba103f63c9c5c06bb412
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-25 19:08:19 +0900

    commit

--- py4.py
+++ py4.py
@@ -12,3 +12,21 @@ eye = np.eye(4)
 print('eye:\n{}'.format(eye))
 
 # %%
+#numpy配列をSciPyのCSRの疎行列に変換する
+#非ゼロ要素だけが格納される
+eye[0, 2] = 5
+print(eye)
+sparse_matrix = sparse.csr_matrix(eye)
+print('sparse_matrix:\n{}'.format(sparse_matrix))
+# %%
+data = np.ones(4)
+row_idices = np.arange(4)
+col_indices = np.arange(4)
+print(data)
+print(row_idices)
+print(col_indices)
+
+# %%
+eye_coo = sparse.coo_matrix((data, (row_idices, col_indices)))
+print(eye_coo)
+# %%
