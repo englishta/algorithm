f55eb115f344a84a673f055a2f9197458a243db6
root <root@DESKTOP-MUVEHV2.localdomain>
2020-11-27 18:19:07 +0900

    commit

--- py4.py
+++ py4.py
@@ -84,5 +84,35 @@ import pandas as pd
 iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset['feature_names'])
 iris_dataframe
 # %%
-import mglearn
-grr = pd.scatter_matrix(iris_dataframe, c=y_train, figsize = (15, 15), marker = 'o', hist_kwds={'bins' : 20}, s = 60, alpha = .8, cmap=mglearn.cm3)
+import mglearn#色のをいい感じにしてくれもの
+import pandas as pd
+pd.plotting.scatter_matrix(iris_dataframe, c = y_train, marker = '.', hist_kwds={'bins':20}, s = 60, alpha = .8, figsize = (8, 8), cmap = mglearn.cm3)
+#c = llll
+#コラムllllによって色分けする
+# %%
+#k近傍法
+k = 5
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=k)
+
+knn.fit(X_train, y_train)
+# %%
+#予測を行う
+#注意！！np.array([[]])括弧の数
+X_new = np.array([[5, 2.9, 1, 0.2]])
+prediction = knn.predict(X_new)
+print(prediction)
+print(iris_dataset['target_names'])
+print(iris_dataset['target_names'][prediction])
+# %%
+#精度
+
+y_pred = knn.predict(X_test)
+print("Test set prediction:\n{}".format(y_pred))
+# %%
+print("Test set score: {:.2f}".format(np.mean(y_pred==y_test)))
+
+# %%
+
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+# %%
