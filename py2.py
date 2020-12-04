af58dab943c1470f04203d219a1cda4b65e9470a
root <root@DESKTOP-MUVEHV2.localdomain>
2020-12-04 11:40:26 +0900

    commit

--- py2.py
+++ py2.py
@@ -1,6 +1,15 @@
 #%%
+#癌の陽性,陰性データ
 from sklearn.datasets import load_breast_cancer
 cancer = load_breast_cancer()
 cancer.keys()
 
+# %%
+print("Shape of cancer data : {}".format(cancer.data.shape))
+
+# %%
+import numpy as np
+{n:v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}
+#クラスごとのサンプルの個数
+#悪性212, 良性357 
 # %%
