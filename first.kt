dc6c3952ee7b434ad58c68e954e1ab8e8024d959
root <root@DESKTOP-MUVEHV2.localdomain>
2020-12-06 04:08:07 +0900

    commit

---
+++ first.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>) {
+    var s = readDouble() 
+    println(s*2)
+}
+
+// region kokokara template dayo (^o^)
+fun readInt() = readLine()!!.toInt()
+fun readLong() = readLine()!!.toLong()
+fun readDouble() = readLine()!!.toDouble()
+fun readStr() = readLine()!!.toString()
