16d1c657686c1529aa39b1ee43d81fec367850a4
root <root@DESKTOP-MUVEHV2.localdomain>
2021-01-19 19:35:38 +0900

    commit

--- py2.py
+++ py2.py
@@ -1,17 +1,66 @@
 #%%
-#癌の陽性,陰性データ
-from sklearn.datasets import load_breast_cancer
-cancer = load_breast_cancer()
-cancer.keys()
-
-# %%
-print("Shape of cancer data : {}".format(cancer.data.shape))
-
-# %%
+import pandas as pd
 import numpy as np
-{n:v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}
-#クラスごとのサンプルの個数
-#悪性212, 良性357 
+train = pd.read_csv("graphic/kaggle/titanik/train.csv")
+test = pd.read_csv("graphic/kaggle/titanik/test.csv")
+
+train.head(3)
 # %%
-cancer.feature_names
+train.info()
+#それぞれのコラムに891個のデータがあることが分かる
+#Age,Cabin,Embarkedにnullがあることが分かる
 # %%
+train2 = train.replace("male", 0).replace("female", 1).replace("S", 0).replace("Q", 2).replace("C", 1)
+test2 = test.replace("male", 0).replace("female", 1).replace("S", 0).replace("Q", 2).replace("C", 1)
+#EmbarkedのSCQを数値に変換
+test.head(10)
+# %%
+
+test2.head(10)
+# %%
+train2["Age"].fillna(train2.Age.mean(), inplace = True)
+train2["Embarked"].fillna(train2.Embarked.mean(), inplace = True)
+test2["Age"].fillna(test2.Age.mean(), inplace = True)
+test2["Embarked"].fillna(test2.Embarked.mean(), inplace = True)
+test2["Fare"].fillna(test2.Fare.mean(), inplace = True)
+train2.info()
+# %%
+#Cabinの処理が面倒なのでカラムから消去
+train2 = train2.drop(["Cabin", "Name", "Ticket"], axis=1)
+test2 = test2.drop(["Cabin", "Name", "Ticket"], axis=1)
+
+test2.head(10)
+# %%
+test2.head(10)
+# %%
+train2.info()
+test2.info()
+
+# %%
+target_train = train2["Survived"]
+feature_train = train2.drop("Survived", axis=1)
+
+target_train
+
+# %%
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+# %%
+model = RandomForestClassifier()
+model.fit(feature_train, target_train)
+#%%
+pred_train = model.predict(feature_train)
+metrics.accuracy_score(target_train, pred_train)
+# %%
+
+pred_test = model.predict(test2)
+# %%
+pred_test
+#%%
+#予測結果をcsvファイルに保存する
+my_prediction = pd.DataFrame(pred_test, test2["PassengerId"], columns=["Survived"])
+my_prediction.to_csv("graphic/kaggle/titanik/my_prediction2.csv", index_label=["PassengerId"])
+# %%
+
