c331a1b537fafa90d02b871fc90fcdac9d924577
root <root@DESKTOP-MUVEHV2.localdomain>
2021-02-13 20:59:20 +0900

    commit

--- prr.cpp
+++ prr.cpp
@@ -97,9 +97,82 @@ vector<T> compress(vector<T> &X){
 /* ------------------------------------------------------------------------- */
 #pragma endregion   
 
-int main() {
-    int a=1;
-    drop(a);
+typedef pair<ll,ll> P;
 
+struct edge{ll to, cost;};
+
+struct graph{
+    ll V;
+    vector<vector<edge> > G;
+    vector<ll> d, ret;
+
+    graph(ll n){
+        init(n);
+    }
+
+    void init(ll n){
+        V = n;
+        G.resize(V);
+        d.resize(V);
+        ret.resize(V);
+        for(ll i=0; i<V; i++){
+            d[i] = INF;
+            ret[i]=INF;
+        }
+    }
+
+    void add_edge(ll s, ll t, ll cost){            
+        edge e;
+        e.to = t, e.cost = cost;
+        G[s].push_back(e);
+    }
+
+    void dijkstra(ll s){
+        for(ll i=0; i<V; i++){
+            d[i] = INF;
+        }
+        d[s] = 0;
+        priority_queue<P,vector<P>, greater<P> > que;
+        que.push(P(0,s));
+
+        while(!que.empty()){
+            P p = que.top(); que.pop();
+            ll v = p.second;
+            if(d[v]<p.first) continue;
+            for(auto e : G[v]){
+                if(d[e.to]>d[v]+e.cost){
+                    d[e.to] = d[v]+e.cost;
+                    que.push(P(d[e.to],e.to));
+                }
+            }
+        }
+    }
+};
+
+int main() {
+
+    // int n = 3;
+    // graph g(n);
+    // g.add_edge(0, 1, 5);
+    // g.add_edge(0, 2, 3);
+    // g.add_edge(2, 1, 1);
+    // g.dijkstra(0);
+    // cout << g.d[1] << endl;
+    LL(n, m);
+    graph g(n+1);
+
+    rep(i,m){
+        LL(a, b, c);
+        g.add_edge(a, b, c);
+    }
+
+    vv(ll, dist, n+1, n+1, 0);
+    rep1(i, 1, n+1){
+        g.dijkstra(i);
+        rep1(J, 1, n+1){
+            if(J==i) continue;
+            dist[i][J] = g.d[J]; 
+        }
+    }
     return 0;
 }
