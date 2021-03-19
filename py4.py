8493e9940cbe1f118f19b7a24a1d1217a81a1140
englishta <1999ju373@gmail.com>
2021-03-19 10:40:01 +0900

    a

--- py4.py
+++ py4.py
@@ -4,7 +4,5 @@ from heapq import heappush, heappop, heappushpop, heapify, heapreplace, merge
 from bisect import bisect_left, bisect_right, bisect, insort_left, insort_right, insort
 from itertools import accumulate, product, permutations, combinations, combinations_with_replacement
 
-n = int(input())
-t=n*n
-s=(n-1)*(n-1)
-print(t/s/2 if n%2==0 else 2*t/s)
+
+print(7**4%5)
