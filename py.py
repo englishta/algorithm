aa47d0202ffcae6c59ab84d3eb4f589777a01695
englishta <1999ju373@gmail.com>
2021-06-19 13:53:41 +0900

    a

--- py.py
+++ py.py
@@ -1,13 +1,6 @@
 from py2 import func
 
-# d = {'hens' : 3, 'x' : 4}
-# s = "int t = hens+2.2;"
-# s = "int a=3, b=2;"
-
-# print(d)
-# func(s, d)
-# print(d)
-d = {'a' : 2, 'b' : 3}
-s = "int c = a+b, d, h = 2*c, x=d*2;"
+d = {'hens' : 2, 'b' : 3}
+s = "int x = hens*2+3, t = x*x;"
 func(s, d)
 print(d)
