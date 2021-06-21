5eea50c2e0464f24921deb813d9bcbab50af5d73
englishta <1999ju373@gmail.com>
2021-06-21 13:17:40 +0900

    a

--- py.py
+++ py.py
@@ -1,7 +1,8 @@
 from py2 import Scan1
 
 d = {'hens' : 2, 'a' : 3, 'aa' : 4}
-# s = "int x = aa*2;"
-s = "int x, y=2*x, t = aa;"
+s = "int t = hen*2"
 Scan1(s, d)
+# s = "int x, y=2*x, t = aa;"
+# Scan1(s, d)
 print(d)
